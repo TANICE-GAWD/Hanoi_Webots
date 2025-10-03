01e364042ea35c89794a48cb55527f7ea164fa9b
TANICE-GAWD <2006princesharma@gmail.com>
2025-10-03 23:49:01 +0530

    Update hanoi.c

--- hanoi.c
+++ hanoi.c
@@ -1,4 +1,30 @@
+/*
+ * Copyright 1996-2024 Cyberbotics Ltd.
+ *
+ * This file was modified by [Your Name] in 2025.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+/*
+ * File: youbot_hanoi_solver.c
+ * Date: October 4, 2025
+ * Description: An interactive and recursive Tower of Hanoi solver for the YouBot robot.
+ * The robot dynamically calculates and executes the necessary moves to solve
+ * the puzzle for a user-specified number of disks (1 to 3).
+ * Author: [Your Name Here]
+ * Original Author: Cyberbotics Ltd.
+ */
 
 /*
  * Description: An interactive, recursive Tower of Hanoi solver.
